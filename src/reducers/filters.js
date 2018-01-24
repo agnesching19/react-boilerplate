40dc2b0c8023201d75e0525c073fafbc2f8c8f00
Agnes Ching <agnesching19@gmail.com>
2018-01-24 18:31:03 +0000

    filter by dates

--- src/reducers/filters.js
+++ src/reducers/filters.js
@@ -1,27 +1,30 @@
+import moment from 'moment';
+
 // Filters Reducer
-const filtersReducersDefaultState = {
+
+const filtersReducerDefaultState = {
   text: '',
   sortBy: 'date',
-  startDate: undefined,
-  endDate: undefined
+  startDate: moment().startOf('month'),
+  endDate: moment().endOf('month')
 };
 
-const filtersReducer = (state = filtersReducersDefaultState, action) => {
+export default (state = filtersReducerDefaultState, action) => {
   switch (action.type) {
     case 'SET_TEXT_FILTER':
       return {
         ...state,
         text: action.text
       };
-    case 'SORT_BY_DATE':
-      return {
-        ...state,
-        sortBy: 'date'
-      };
     case 'SORT_BY_AMOUNT':
       return {
         ...state,
         sortBy: 'amount'
+      };
+    case 'SORT_BY_DATE':
+      return {
+        ...state,
+        sortBy: 'date'
       };
     case 'SET_START_DATE':
       return {
@@ -30,12 +33,10 @@ const filtersReducer = (state = filtersReducersDefaultState, action) => {
       };
     case 'SET_END_DATE':
       return {
-         ...state,
-         endDate: action.endDate
+        ...state,
+        endDate: action.endDate
       };
     default:
       return state;
   }
 };
-
-export default filtersReducer;
