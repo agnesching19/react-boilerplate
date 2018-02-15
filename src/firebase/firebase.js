8f4860d31bd54ffa9cedd82b255128b1cd3d8b56
Agnes Ching <agnesching19@gmail.com>
2018-02-15 18:22:52 +0000

    fetching data from firebase

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,29 +13,64 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref().set({
-  name: 'Agnes Ching',
-  age: 20,
-  stressLevel: 8,
-  job: {
-    title: 'Software Developer',
-    company: 'Google'
-  },
-  location: {
-    city: 'London',
-    country: 'United Kingdom'
-  }
-}).then(() => {
-  console.log('Data is saved');
-}).catch((error) => {
-  console.log('This failed', error);
+const onValueChange = database.ref().on('value', (snapshot) => {
+  const val = snapshot.val();
+  console.log(`${val.name} is a ${val.job.title} at ${val.job.company}`);
+}, (error) => {
+  console.log('Error with data fetching', error);
 });
 
-database.ref().update({
-  stressLevel: 6,
-  'job/company': 'Amazon',
-  'location/city': 'Seattle'
-});
+// const onValueChange = database.ref().on('value', (snapshot) => {
+//   console.log(snapshot.val());
+// }, (e) => {
+//   console.log('Error with data fetching', e);
+// });
+
+// setTimeout(() => {
+//   database.ref('age').set(30);
+// }, 3500);
+
+// setTimeout(() => {
+//   database.ref().off(onValueChange);
+// }, 7000);
+
+// setTimeout(() => {
+//   database.ref('age').set(22);
+// }, 10500);
+
+// database.ref('location/country')
+//   .once('value')
+//   .then((snapshot) => {
+//     const val = snapshot.val();
+//     console.log(val);
+//   })
+//   .catch((error) => {
+//     console.log('Error fetching data.', error);
+//   });
+
+// database.ref().set({
+//   name: 'Agnes Ching',
+//   age: 20,
+//   stressLevel: 8,
+//   job: {
+//     title: 'Software Developer',
+//     company: 'Google'
+//   },
+//   location: {
+//     city: 'London',
+//     country: 'United Kingdom'
+//   }
+// }).then(() => {
+//   console.log('Data is saved');
+// }).catch((error) => {
+//   console.log('This failed', error);
+// });
+
+// database.ref().update({
+//   stressLevel: 6,
+//   'job/company': 'Amazon',
+//   'location/city': 'Seattle'
+// });
 
 // database.ref('isGirl').set(null);
 
