318890deef543c17e24a5ce1806f98f418601560
Agnes Ching <agnesching19@gmail.com>
2018-02-16 12:32:39 +0000

    created a seperate test database

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,12 +1,12 @@
 import * as firebase from 'firebase';
 
 const config = {
-  apiKey: "AIzaSyDZiOBFHPhL2eTV-VBkkpQOCZ7wssLPdO0",
-  authDomain: "expensify-7fc18.firebaseapp.com",
-  databaseURL: "https://expensify-7fc18.firebaseio.com",
-  projectId: "expensify-7fc18",
-  storageBucket: "expensify-7fc18.appspot.com",
-  messagingSenderId: "280545481821"
+  apiKey: process.env.FIREBASE_APY_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
 };
 
 firebase.initializeApp(config);
