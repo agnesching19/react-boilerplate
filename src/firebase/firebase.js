3360207566ba271cbc1008185a3fbef6575766e6
Agnes Ching <agnesching19@gmail.com>
2018-02-16 10:16:10 +0000

    asychronous Redux actions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -13,24 +13,26 @@ firebase.initializeApp(config);
 
 const database = firebase.database();
 
-database.ref('expenses').on('child_removed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+export { firebase, database as default};
 
-database.ref('expenses').on('child_changed', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+// database.ref('expenses').on('child_removed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-database.ref('expenses').on('child_added', (snapshot) => {
-  console.log(snapshot.key, snapshot.val());
-});
+// database.ref('expenses').on('child_changed', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
 
-database.ref('expenses').push({
-  description: 'Beer',
-  note: '',
-  amount: 350,
-  createdAt: 964138744
-});
+// database.ref('expenses').on('child_added', (snapshot) => {
+//   console.log(snapshot.key, snapshot.val());
+// });
+
+// database.ref('expenses').push({
+//   description: 'Beer',
+//   note: '',
+//   amount: 350,
+//   createdAt: 964138744
+// });
 
 // database.ref('expenses')
 //   .once('value')
