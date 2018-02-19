c98d596daaa31e74c67d04d94d4b42e1fe0d6d7e
Agnes Ching <agnesching19@gmail.com>
2018-02-19 12:25:39 +0000

    google auth added

---
+++ src/components/LoginPage.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { startLogin } from '../actions/auth';
+
+export const LoginPage = ({startLogin}) => (
+  <div>
+    <button onClick={startLogin}>Login</button>
+  </div>
+);
+
+const mapDispatchToProps = (dispatch) => ({
+  startLogin: () => dispatch(startLogin())
+});
+
+export default connect(undefined, mapDispatchToProps)(LoginPage);
+
+// export class LoginPage extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         <button>Login</button>
+//       </div>
+//     );
+//   }
+// };
--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,19 +1,29 @@
 import * as firebase from 'firebase';
 
+// const config = {
+//   apiKey: process.env.FIREBASE_APY_KEY,
+//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+//   databaseURL: process.env.FIREBASE_DATABASE_URL,
+//   projectId: process.env.FIREBASE_PROJECT_ID,
+//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+// };
+
 const config = {
-  apiKey: process.env.FIREBASE_APY_KEY,
-  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-  databaseURL: process.env.FIREBASE_DATABASE_URL,
-  projectId: process.env.FIREBASE_PROJECT_ID,
-  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID
+  apiKey: "AIzaSyDZiOBFHPhL2eTV-VBkkpQOCZ7wssLPdO0",
+  authDomain: "expensify-7fc18.firebaseapp.com",
+  databaseURL: "https://expensify-7fc18.firebaseio.com",
+  projectId: "expensify-7fc18",
+  storageBucket: "expensify-7fc18.appspot.com",
+  messagingSenderId: "280545481821"
 };
 
 firebase.initializeApp(config);
 
 const database = firebase.database();
+const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
 
-export { firebase, database as default};
+export { firebase, googleAuthProvider, database as default };
 
 // database.ref('expenses').on('child_removed', (snapshot) => {
 //   console.log(snapshot.key, snapshot.val());
